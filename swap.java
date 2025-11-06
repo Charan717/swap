d445ff6de6c946a98532651fabebb7bc5a14a139
Charan717 <saicharangolkonda34@gmail.com>
2025-11-06 14:32:54 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+class swap {
+public static void main(String[] args) {
+int a = 10;
+int b = 5;
+int temp;
+temp = a;
+a = b;
+b = temp;
+System.out.println("After swapping, a = " + a + " and b = " + b);
+    }
+}
